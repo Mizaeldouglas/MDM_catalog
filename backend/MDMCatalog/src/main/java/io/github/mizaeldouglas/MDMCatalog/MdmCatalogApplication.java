814353cb8b01cae7c90c3778ec00ef6c0b15308b
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-09-07 12:27:18 -0300

    creating project

---
+++ backend/MDMCatalog/src/main/java/io/github/mizaeldouglas/MDMCatalog/MdmCatalogApplication.java
@@ -0,0 +1,13 @@
+package io.github.mizaeldouglas.MDMCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/MDMCatalog/src/test/java/io/github/mizaeldouglas/MDMCatalog/MdmCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mizaeldouglas.MDMCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
